8927dbeb1ae4ff22ecc876789931dfd38643ed60
Falk <julien.delusseau@gmail.com>
2020-07-14 12:25:05 +0200

    Added sign-page and sign-in form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/HomePage'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shoppage/ShopPage'
 import Header from './components/header/Header'
+import SignInAndUpPage from './pages/signin-signup/SignInAndUpPage'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/signin' component={SignInAndUpPage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </Fragment>
